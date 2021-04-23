e19280214664a9f694629d0c336f0095cf9038ed
Encho Topalov <entopalov@gmail.com>
2021-04-23 03:01:20 +0300

    4.4.1 Styling AppBar

--- places/lib/ui/screen/sight_list_screen.dart
+++ places/lib/ui/screen/sight_list_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class SightListScreen extends StatefulWidget {
   SightListScreen({Key key}) : super(key: key);
 
-  final String title = "Список интересных мест";
+  final String title = "Список\nинтересных мест";
+  final double paddingTop = 40.0;
 
   @override
   _SightListScreenState createState() => _SightListScreenState();
@@ -14,16 +15,27 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        shadowColor: Colors.transparent,
+        title: Padding(
+          child: Text(
+            widget.title,
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: "RobotoRegular",
+                fontSize: 32.0,
+                fontWeight: FontWeight.w700,
+                height: 1.125),
+            textAlign: TextAlign.left,
+            overflow: TextOverflow.ellipsis,
+          ),
+          padding: EdgeInsets.fromLTRB(0, widget.paddingTop, 0, 0.0),
+        ),
+        toolbarHeight: 72.0 + widget.paddingTop,
       ),
       resizeToAvoidBottomInset: false,
-      body: Center(child: TextField()),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Btn1'),
-          BottomNavigationBarItem(icon: Icon(Icons.alarm), label: 'Btn2')
-        ],
-      ),
+      body: Center(child: Text('body')),
     );
   }
 }
