5dd8b366ff008306e29f006cc23020abbe7b1bae
Encho Topalov <entopalov@gmail.com>
2021-04-27 00:06:41 +0300

    4.4.2: Add colors and styles

---
+++ places/lib/ui/res/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+// Семантические алиасы
+// Пополняются по мере необходимости
+// Используют только основную палитру
+const Color colorAccent = _green, colorAccent2 = _yellow, textColorPrimary = _black;
+
+// Основная палитра (http://www.color-blindness.com/color-name-hue/)
+const Color _yellow = Color(0xFFFCDD3D), _green = Color(0xFF4CAF50), _black = Color(0xFF000000);
