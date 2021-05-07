e7a8f07e7c256ad5ff12f81938d6592ee1c98b14
Encho Topalov <entopalov@gmail.com>
2021-05-07 03:41:04 +0300

    4.6.2 Update sight list screen

--- places/lib/ui/screen/sight_list_screen.dart
+++ places/lib/ui/screen/sight_list_screen.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:places/ui/res/text_styles.dart';
-import 'package:places/ui/res/colors.dart';
+
+import 'sight_card.dart';
+
+import 'package:places/mocks.dart';
 
 class SightListScreen extends StatefulWidget {
-  SightListScreen({Key key}) : super(key: key);
-
   final String titleLine1 = "Список";
   final String titleLine2 = "интересных мест";
   final double paddingTop = 40.0;
+
+  SightListScreen({Key key}) : super(key: key);
 
   @override
   _SightListScreenState createState() => _SightListScreenState();
 }
 
 class _SightListScreenState extends State<SightListScreen> {
+  List<SightCard> _cards = [];
+
+  _SightListScreenState() {
+    _cards = mocks.map((sight) => SightCard(sight)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,25 +30,29 @@ class _SightListScreenState extends State<SightListScreen> {
         elevation: 0.0,
         backgroundColor: Colors.transparent,
         shadowColor: Colors.transparent,
-        title: Padding(
+        title: Container(
           child: RichText(
             text: TextSpan(
-              text: widget.titleLine1.substring(0, 1),
-              style: textAppBarAccent1,
+              text: widget.titleLine1,
+              style: textAppBar,
               children: [
-                TextSpan(text: widget.titleLine1.substring(1), style: textAppBar),
-                TextSpan(text: "\n${widget.titleLine2.substring(0, 1)}", style: textAppBarAccent2),
-                TextSpan(text: widget.titleLine2.substring(1), style: textAppBar)
+                TextSpan(text: "\n${widget.titleLine2}", style: textAppBar),
               ],
             ),
             overflow: TextOverflow.ellipsis,
           ),
-          padding: EdgeInsets.fromLTRB(0, widget.paddingTop, 0, 0.0),
+          padding: EdgeInsets.fromLTRB(0.0, widget.paddingTop, 0.0, 16.0),
         ),
-        toolbarHeight: 72.0 + widget.paddingTop,
+        toolbarHeight: 88.0 + widget.paddingTop,
       ),
       resizeToAvoidBottomInset: false,
-      body: Center(child: Text('body')),
+      body: SingleChildScrollView(
+        child: Wrap(
+          runSpacing: 16,
+          children: _cards,
+        ),
+        padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+      ),
     );
   }
 }
