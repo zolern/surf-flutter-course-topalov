5db59ddc3b3a312e13c97d76d1dc1b7789aa1e90
Encho Topalov <entopalov@gmail.com>
2021-05-16 13:30:24 +0300

    4.8: Use SightListScreen as main screen

--- places/lib/main.dart
+++ places/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'ui/screen/sight_list_screen.dart';
-import 'ui/screen/sight_details.dart';
+import 'ui/screen/sight_list_screen.dart';
 import 'package:places/mocks.dart';
 
 void main() {
@@ -9,9 +8,8 @@ void main() {
 }
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SightDetails(mocks[1]), title: 'My Flutter Demo 4.6');
+    return MaterialApp(home: SightListScreen(), title: 'My Flutter Demo 4.8');
   }
 }
