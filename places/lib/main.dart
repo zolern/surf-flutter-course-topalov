7f6d676bf8c338eb1dd5777bea3b01c1d92f8089
Encho Topalov <entopalov@gmail.com>
2021-05-18 22:11:21 +0300

    Use SightDetails as main screen

--- places/lib/main.dart
+++ places/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'ui/screen/sight_list_screen.dart';
+import 'ui/screen/sight_details.dart';
 import 'package:places/mocks.dart';
 
 void main() {
@@ -10,6 +10,6 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SightListScreen(), title: 'My Flutter Demo 4.8');
+    return MaterialApp(home: SightDetailsScreen(sight: mocks[3]), title: 'My Flutter Demo 4.8');
   }
 }
