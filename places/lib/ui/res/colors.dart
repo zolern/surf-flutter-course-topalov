ae75d0bf7942507ae6a986ac1410ccf60e4ede03
Encho Topalov <entopalov@gmail.com>
2021-05-18 13:02:31 +0300

    4.10.1 Update assets, colors and styles
    * add images folder to assets
    * add heart icon to images
    * add new file with decorations
    * update colors, add gradient const

--- places/lib/ui/res/colors.dart
+++ places/lib/ui/res/colors.dart
@@ -10,15 +10,26 @@ const Color textWHITEGreen = _green,
     textWhite = _white,
     textBLACKMain = _blackRussian;
 
+const Color colorWHITEAccent = _green, colorSemiSecondary2 = _slateGreySemiTransparent; //0.56 opacity Slate Grey
+
 const Color bgBackground = _whiteSmoke, bgDefault = _white;
 
-const Color colorSemiSecondary2 = const Color.fromRGBO(124, 126, 146, 0.56); //0.56 opacity Slate Grey
+const LinearGradient bgLinearGradient = const LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [
+    _luckyPoint, // #252849 at 100%
+    _blueZodiacTransparent, // #3B3E5B at 8%
+  ],
+);
 
 // Основная палитра (http://www.color-blindness.com/color-name-hue/)
 const Color _green = Color(0xFF4CAF50),
     _luckyPoint = Color(0xFF252849),
     _blueZodiac = Color(0xFF3B3E5B),
+    _blueZodiacTransparent = const Color.fromRGBO(0x3B, 0x3E, 0x5B, 0.08),
     _white = Color(0xFFFFFFFF),
     _whiteSmoke = Color(0xFFF5F5F5),
     _slateGrey = Color(0xFF7C7E92),
+    _slateGreySemiTransparent = const Color.fromRGBO(0x7C, 0x7E, 0x92, 0.56),
     _blackRussian = Color(0xFF21222C);
